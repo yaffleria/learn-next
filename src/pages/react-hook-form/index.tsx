2784bec3816111c79a5e1e4d2316829a887cb26a
Jungmin Ji <jungmin.ji@icloud.com>
2023-11-19 16:46:04 +0900

    Feat: react-hook-form useFieldArray

--- src/pages/react-hook-form/index.tsx
+++ src/pages/react-hook-form/index.tsx
@@ -1,44 +1,55 @@
 import { NextPage } from "next";
 import dynamic from "next/dynamic";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 
 import styles from "./form.module.css";
 
 type FormValues = {
-  username: string
-  email: string
-  channel: string
+  username: string;
+  email: string;
+  channel: string;
   social: {
-    twitter: string
-    facebook: string
-  }
-  phoneNumbers: string[]
+    twitter: string;
+    facebook: string;
+  };
+  phoneNumbers: string[];
+  phNumbers: {
+    number: string;
+  }[];
 };
 
 const DevTool: React.ElementType = dynamic(
-  () => import('@hookform/devtools').then((module) => module.DevTool),
+  () => import("@hookform/devtools").then((module) => module.DevTool),
   { ssr: false }
-)
+);
 
 const Tester: NextPage = () => {
   const { register, handleSubmit, control, formState } = useForm<FormValues>({
     defaultValues: async () => {
-      const response = await fetch('https://jsonplaceholder.typicode.com/users/1')
-      const data = await response.json()
-      
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users/1"
+      );
+      const data = await response.json();
+
       return {
-        username: 'Batman',
+        username: "Batman",
         email: data.email,
-        channel: '',
+        channel: "",
         social: {
-          twitter: '',
-          facebook: ''
+          twitter: "",
+          facebook: "",
         },
-        phoneNumbers: ['', '']
-      }
-    }
+        phoneNumbers: ["", ""],
+        phNumbers: [{ number: "" }],
+      };
+    },
   });
-  const { errors } = formState
+  const { errors } = formState;
+
+  const { fields, append, remove } = useFieldArray({
+    name: "phNumbers",
+    control,
+  });
 
   const onSubmit = (data: FormValues) => {
     console.log("Form submitted", data);
@@ -51,7 +62,9 @@ const Tester: NextPage = () => {
         onSubmit={handleSubmit(onSubmit)}
         noValidate
       >
-        <label className={styles.label} htmlFor="username">Username</label>
+        <label className={styles.label} htmlFor="username">
+          Username
+        </label>
         <input
           type="text"
           id="username"
@@ -61,7 +74,9 @@ const Tester: NextPage = () => {
         />
         <p className={styles.error}>{errors.username?.message}</p>
 
-        <label className={styles.label} htmlFor="email">Email</label>
+        <label className={styles.label} htmlFor="email">
+          Email
+        </label>
         <input
           type="email"
           id="email"
@@ -72,39 +87,98 @@ const Tester: NextPage = () => {
             },
             validate: {
               notAdmin: (fieldValue) => {
-                return (fieldValue !== 'admin@example.com' || "Enter a different email address")
+                return (
+                  fieldValue !== "admin@example.com" ||
+                  "Enter a different email address"
+                );
               },
               notBlackList: (fieldValue) => {
-                return !fieldValue.endsWith('baddomain.com') || "This domain is not supported"
-              }
-            }
+                return (
+                  !fieldValue.endsWith("baddomain.com") ||
+                  "This domain is not supported"
+                );
+              },
+            },
           })}
         />
         <p className={styles.error}>{errors.email?.message}</p>
 
-        <label className={styles.label} htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register("channel", {
-          required: { value: true, message: 'Channel is required' }
-        })} />
+        <label className={styles.label} htmlFor="channel">
+          Channel
+        </label>
+        <input
+          type="text"
+          id="channel"
+          {...register("channel", {
+            required: { value: true, message: "Channel is required" },
+          })}
+        />
         <p className={styles.error}>{errors.channel?.message}</p>
 
-        <label className={styles.label} htmlFor="twitter">Twitter</label>
-        <input type="text" id="twitter" {...register("social.twitter", {
-          required: { value: true, message: 'Twitter is required' }
-        })} />
+        <label className={styles.label} htmlFor="twitter">
+          Twitter
+        </label>
+        <input
+          type="text"
+          id="twitter"
+          {...register("social.twitter", {
+            required: { value: true, message: "Twitter is required" },
+          })}
+        />
         <p className={styles.error}>{errors.social?.twitter?.message}</p>
 
-        <label className={styles.label} htmlFor="facebook">Facebook</label>
-        <input type="text" id="facebook" {...register("social.facebook", {
-          required: { value: true, message: 'Facebook is required' }
-        })} />
+        <label className={styles.label} htmlFor="facebook">
+          Facebook
+        </label>
+        <input
+          type="text"
+          id="facebook"
+          {...register("social.facebook", {
+            required: { value: true, message: "Facebook is required" },
+          })}
+        />
         <p className={styles.error}>{errors.social?.facebook?.message}</p>
 
-        <label className={styles.label} htmlFor="primary-phone">Primary phone number</label>
+        <label className={styles.label} htmlFor="primary-phone">
+          Primary phone number
+        </label>
         <input type="text" id="primary-phone" {...register("phoneNumbers.0")} />
 
-        <label className={styles.label} htmlFor="secondary-phone">Secondary phone number</label>
-        <input type="text" id="secondary-phone" {...register("phoneNumbers.1")} />
+        <label className={styles.label} htmlFor="secondary-phone">
+          Secondary phone number
+        </label>
+        <input
+          type="text"
+          id="secondary-phone"
+          {...register("phoneNumbers.1")}
+        />
+
+        <div>
+          <label>List of phone numbers</label>
+          <div>
+            {fields.map((field, index) => {
+              return (
+                <div key={field.id}>
+                  <input
+                    type="text"
+                    {...register(`phNumbers.${index}.number`)}
+                  />
+                  {index > 0 && (
+                    <button
+                      type="button"
+                      onClick={() => remove(index)}
+                    >
+                      Remove phone number
+                    </button>
+                  )}
+                </div>
+              );
+            })}
+            <button type="button" onClick={() => append({ number: "" })}>
+              Add phone number
+            </button>
+          </div>
+        </div>
 
         <button className={styles.button}>Submit</button>
       </form>
